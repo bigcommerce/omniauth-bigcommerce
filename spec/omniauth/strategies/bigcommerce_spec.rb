79041e7f4d20891443d3454170368a4b74bdc08e
Sasha Gerrand <github-omniauth-bigcommerce@sgerrand.com>
2016-04-16 19:43:30 +0100

    Nest contexts correctly

--- spec/omniauth/strategies/bigcommerce_spec.rb
+++ spec/omniauth/strategies/bigcommerce_spec.rb
@@ -13,29 +13,31 @@ RSpec.describe OmniAuth::Strategies::BigCommerce do
     OmniAuth.config.test_mode = false
   end
 
-  context 'client options' do
+  context 'options' do
     it 'should have correct name' do
       expect(subject.options.name).to eq('bigcommerce')
     end
 
-    it 'should have correct site' do
-      # env variable set in spec_helper.rb
-      # TODO: change this once we have bigcommerceapp.com url
-      expect(subject.options.client_options.site).to eq('https://example.com')
+    context 'client options' do
+      it 'should have correct site' do
+        # env variable set in spec_helper.rb
+        # TODO: change this once we have bigcommerceapp.com url
+        expect(subject.options.client_options.site).to eq('https://example.com')
+      end
+
+      it 'should have correct authorize url' do
+        expect(subject.options.client_options.authorize_url).to eq('/oauth2/authorize')
+      end
+
+      it 'should have correct token url' do
+        expect(subject.options.client_options.token_url).to eq('/oauth2/token')
+      end
     end
 
-    it 'should have correct authorize url' do
-      expect(subject.options.client_options.authorize_url).to eq('/oauth2/authorize')
-    end
-
-    it 'should have correct token url' do
-      expect(subject.options.client_options.token_url).to eq('/oauth2/token')
-    end
-  end
-
-  context 'oauth2 settings' do
-    it 'should ignore state' do
-      expect(subject.options.provider_ignores_state).to eq true
+    context 'OAuth2 settings' do
+      it 'should ignore state' do
+        expect(subject.options.provider_ignores_state).to eq true
+      end
     end
   end
 
