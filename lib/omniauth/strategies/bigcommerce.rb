082962acf9f565c32bdd39d47cf41872189738fb
Carson Reinke <carson@reinke.co>
2016-04-25 13:46:58 -0400

    Ignore query string for callback url since BigCommerce does not ignore it for redirect_uri Adding coverage folder to .gitignore

--- lib/omniauth/strategies/bigcommerce.rb
+++ lib/omniauth/strategies/bigcommerce.rb
@@ -48,6 +48,11 @@ module OmniAuth
           end
         end
       end
+      
+      #Exclude query string in callback
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
